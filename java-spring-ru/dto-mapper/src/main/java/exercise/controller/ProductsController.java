e6678ea4b377d17ae679802c45875100725965f8
@hexlet/cli <support@hexlet.io>
2024-02-25 12:26:25 +0300

    submit java-spring-ru/dto-mapper

---
+++ java-spring-ru/dto-mapper/src/main/java/exercise/controller/ProductsController.java
@@ -0,0 +1,73 @@
+package exercise.controller;
+
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import java.util.List;
+
+import exercise.repository.ProductRepository;
+import exercise.dto.ProductDTO;
+import exercise.dto.ProductCreateDTO;
+import exercise.dto.ProductUpdateDTO;
+import exercise.exception.ResourceNotFoundException;
+import exercise.mapper.ProductMapper;
+
+@RestController
+@RequestMapping("/products")
+public class ProductsController {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    // BEGIN
+    //GET /products – вывод списка всех товаров
+    //GET /products/{id} – просмотр конкретного товара
+    //POST /products – создание нового товара
+    //PUT /products/{id} – редактирование товара
+
+    @Autowired
+    private ProductMapper productMapper;
+
+    @GetMapping(path = "")
+    @ResponseStatus(HttpStatus.OK)
+    public List<ProductDTO> show() {
+        var products = productRepository.findAll();
+        return products.stream().map( productMapper::map).toList();
+    }
+
+    @GetMapping(path = "/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public ProductDTO showProduct(@PathVariable long id) {
+        var product = productRepository.findById(id)
+                .orElseThrow(()->new ResourceNotFoundException("Product Not Found"));
+        return productMapper.map(product);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ProductDTO create(@RequestBody ProductCreateDTO productCreateDTO) {
+        var product = productMapper.map(productCreateDTO);
+        productRepository.save(product);
+        return productMapper.map(product);
+    }
+
+    @PutMapping(path = "/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public ProductDTO update(@PathVariable long id, @RequestBody ProductUpdateDTO productUpdateDTO) {
+        var product = productRepository.findById(id)
+                .orElseThrow(()-> new ResourceNotFoundException("Product Not Found"));
+        productMapper.update(productUpdateDTO, product);
+        productRepository.save(product);
+        return productMapper.map(product);
+    }
+    
+    // END
+}
